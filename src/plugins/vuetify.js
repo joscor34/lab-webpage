d83a53485f59253be5ab95a2a76060f175e1247e
Ximenakdi <ximena_pinacho@icloud.com>
2022-10-01 18:55:23 -0500

    Se añadieron nuevos colores al tema de vue

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,22 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        FIRed: '#B61922',
+        FIGray: '#595959',
+        babyFIGray: '#D1D2D4',
+        popPink: '#E73567',
+        sunnyYellow: '#F9B956'
+      },
+      dark: {
+        FIRed: '#B61922',
+        FIGray: '#595959',
+        babyFIGray: '#D1D2D4',
+        popPink: '#E73567',
+        sunnyYellow: '#F9B956'
+      }
+    }
+  }
 })
