6e1d801341fb9ba299895bbb6bb03e3639afa301
joscor34 <zbrm34@gmail.com>
2022-10-01 01:04:05 -0500

    Se creo store para la app

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import Axios from 'axios'
+// import { reject, resolve } from 'core-js/fn/promise'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: null,
+    user: null
   },
   getters: {
+
   },
   mutations: {
+    setUser (state, user) {
+      state.user = user
+    },
+    setToken (state, token) {
+      state.token = token
+    }
   },
   actions: {
+    LOGIN: () => {
+      return new Promise((resolve, reject) => {})
+    }
   },
   modules: {
   }
