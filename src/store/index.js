88076f6e5a4fd731fdea540d95ba8127d4d69fd8
joscor34 <zbrm34@gmail.com>
2022-10-01 01:34:18 -0500

    Se creó el metodo para LOGIN y REGISTER en store de vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import Axios from 'axios'
-// import { reject, resolve } from 'core-js/fn/promise'
+import axios from 'axios'
 
 Vue.use(Vuex)
+
+const baseurl = 'http://localhost:8080/api/'
 
 export default new Vuex.Store({
   state: {
@@ -22,8 +23,27 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    LOGIN: () => {
-      return new Promise((resolve, reject) => {})
+    LOGIN: ({ commit }, { email, password }) => {
+      return new Promise((resolve, reject) => {
+        axios.post(baseurl + 'user/login', { email, password }).then((data, status) => {
+          if (status === 200) {
+            resolve(true)
+          }
+        }).catch(err => {
+          reject(err)
+        })
+      })
+    },
+    REGISTER: ({ commit }, { firstName, lastName, email, password, phoneNumber }) => {
+      return new Promise((resolve, reject) => {
+        axios.post(baseurl + 'user/login', { firstName, lastName, email, password, phoneNumber }).then((data, status) => {
+          if (status === 200) {
+            resolve(true)
+          }
+        }).catch(err => {
+          reject(err)
+        })
+      })
     }
   },
   modules: {
