19cc1c78d52d4fdc4e3d0818a930e797bd2ca8bc
joscor34 <zbrm34@gmail.com>
2022-10-05 17:06:55 -0500

    Se creo un metodo para carga con el progress bar

--- src/components/adminPanelComp.vue
+++ src/components/adminPanelComp.vue
@@ -1,10 +1,39 @@
 <template>
-	<h1>This is admin panel</h1>
+	<v-container>
+		<v-row justify="center" align="center" v-if="carga">
+			<v-progress-circular
+				:size="70"
+				:width="7"
+				color="FIRed"
+				indeterminate
+			></v-progress-circular>
+		</v-row>
+		<p v-else v-for="(proyecto, idx) in proyectos" :key="idx">{{idx}}: {{proyecto.title}} - {{proyecto.abstract}}</p>
+	</v-container>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    proyectos: [],
+    error: '',
+    carga: false
+  }),
+  created () {
+    this.carga = true
+    this.getProyects()
+  },
+  methods: {
+    getProyects () {
+      this.$store.dispatch('GET_PROYECTS').then(success => {
+        this.proyectos = success.data
+        this.carga = false
+      }).catch(err => {
+        this.error = err
+        this.carga = false
+      })
+    }
+  }
 }
 </script>
 
