726682b889f253acd9d065bcae5810701a830a10
Ximenakdi <ximena_pinacho@icloud.com>
2022-10-05 21:00:14 -0500

    Formato para revisar formatos individualmente, LISTO

--- src/components/proyectComp.vue
+++ src/components/proyectComp.vue
@@ -1,5 +1,63 @@
 <template>
-  <h1>Esta pagina la pertenece al proyecto con id: {{$route.params.id}}</h1>
+  <v-container>
+    <v-row justify="center" align="start">
+      <v-col cols="12" sm="10">
+        <!-- AQUÍ VA EL TITULO -->
+        <h1>The effect of thermal treatments in resistivity of a graphene oxide</h1>
+        <!-- COORDINADOR -->
+        <h5>Coordinado por Dr. Arturo Méndez López</h5>
+        <!-- FECHA Y HORA EN LA QUE SE SUBIÓ EL PROYECTO -->
+        <div class="FIGray--text">
+          <h6>Fecha de entrega: 05 de abril de 2022, 19:29</h6>
+          <h6>Última modificación: 05 de abril de 2022, 19:29</h6>
+        </div>
+        <v-divider class="mb-2 mt-2"></v-divider>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="start">
+      <v-col cols="12" sm="7">
+        <!-- AQUÍ VA EL ABSTRACT -->
+        <p>
+          You know what? It is beets. I've crashed into a beet truck. You know what? It is beets. I've crashed into a beet truck. Forget the fat lady! You're obsessed with the fat lady! Drive us out of here! Yes, Yes, without the oops! God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.
+        </p>
+        <p>
+          Did he just throw my cat out of the window? My dad once told me, laugh and the world laughs with you, Cry, and I'll give you something to cry about you little bastard! I gave it a cold? I gave it a virus. A computer virus. I gave it a cold? I gave it a virus. A computer virus.
+        </p>
+        <p>
+          You really think you can fly that thing? My dad once told me, laugh and the world laughs with you, Cry, and I'll give you something to cry about you little bastard! Did he just throw my cat out of the window? Hey, you know how I'm, like, always trying to save the planet? Here's my chance.
+        </p>
+        <p>
+          So you two dig up, dig up dinosaurs? Jaguar shark! So tell me - does it really exist? Just my luck, no ice. Just my luck, no ice. We gotta burn the rain forest, dump toxic waste, pollute the air, and rip up the OZONE! 'Cause maybe if we screw up this planet enough, they won't want it anymore!
+        </p>
+      </v-col>
+      <v-col cols="12" sm="3">
+        <v-card elevation="0" color="#edede9" shaped>
+          <div class="pa-6 mb-4">
+            <div class="text-overline FIRed--text">Palabras clave:</div>
+            <div class="body-2">
+              Microplastics, PAHs, PBCs, DDTs, Plastic pellets
+            </div>
+            <div class="text-overline FIRed--text mt-2">Autores:</div>
+            <div class="body-2">
+              <p><strong>Nombre:</strong> Ali Inés Gómez Acosta<br>
+              <strong>País:</strong> México<br>
+              <strong>Institución:</strong> UAQ</p>
+              <v-divider class="mb-4"></v-divider>
+              <p><strong>Nombre:</strong> Alejandro Manzano Ramírez<br>
+              <strong>País:</strong> México<br>
+              <strong>Institución:</strong> UAQ</p>
+            </div>
+            <div class="body-2 mt-4">
+              <v-icon color="FIRed">
+                mdi-paperclip
+              </v-icon>
+              <a href="https://sci-hub.se/https://doi.org/10.1016/j.marpolbul.2010.07.030" class="FIRed--text" style="text-decoration: none;" target="_blank">proyectoFinal.pdf</a>
+            </div>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -9,5 +67,7 @@ export default {
 </script>
 
 <style>
-
+/* .v-divider {
+  border-color: rgb(182, 25, 33) !important;
+} */
 </style>
