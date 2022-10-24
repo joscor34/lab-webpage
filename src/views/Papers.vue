475f30ba3a55c1a8543689171956e6506d57bc30
Ximenakdi <ximena_pinacho@icloud.com>
2022-10-23 19:39:46 -0500

    Se añadió la pagina que muestra todos los articulos publicados

---
+++ src/views/Papers.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="10">
+        <PaperCard/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import PaperCard from '../components/paperCard.vue'
+
+export default {
+  name: 'Papers',
+  components: {
+    PaperCard
+  }
+}
+</script>
+
+<style>
+
+</style>
