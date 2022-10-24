2d594605f72113d9b4c9e68831ab719731fa79f3
Ximenakdi <ximena_pinacho@icloud.com>
2022-10-24 02:13:17 -0500

    Se cambió el diseño de las tarjetas, ahora muestran el nombre del autor, año de publicación, una imagen del articulo y tiene elevación al posicionar el mouse sobre ella

--- src/components/paperCard.vue
+++ src/components/paperCard.vue
@@ -2,17 +2,18 @@
   <v-container>
     <v-row align="center" justify="center">
       <v-col cols="12" sm="10">
-        <v-hover v-slot="{ hover }" close-delay="200">
-          <v-card color="#f2f2f2" :elevation="hover ? 16 : 0">
+        <v-hover v-slot:default="{ hover }">
+          <v-card color="#f2f2f2" :class="`elevation-${hover ? 24 : 0}`" class="transition-swing">
             <v-row align="center" justify="center" class="ma-4 pa-4">
               <v-col cols="12" sm="8">
+                <p class="caption font-weight-medium">2009</p>
                 <h2>Polymeric nanomedicine for cancer MR imaging and drug delivery</h2>
-                <v-btn tile class="my-4" elevation="0" dark color="FIRed">Leer artículo</v-btn>
+                <p class="mt-1 body-2 font-weight-light">Chalermchai Khemtong, Chase W. Kessinger y Jinming Gao</p>
+                <v-btn tile :class="`elevation-${hover ? 24 : 0}`" class="my-4 transition-swing" dark color="FIRed">Leer artículo</v-btn>
               </v-col>
               <v-col cols="12" sm="4">
                 <div class="text-center">
-                  <v-img class="rounded-circle mx-auto" height="150" width="150" src="https://www.gameshub.com/wp-content/uploads/sites/5/2022/03/bella-goth-sims-2-disappearance.jpg"></v-img>
-                  <h3 class="mt-2">Dra. Bella Goth</h3>
+                  <v-img :class="`elevation-${hover ? 24 : 0}`" class="transition-swing rounded-circle mx-auto" height="150" width="150" src="https://faculty.mdanderson.org/content/dam/mdanderson/images/publications/cancerwise/N/xNature_Nano_nBiPE_Art.JPG.resize.jpg.pagespeed.ic.rWAoFE5Lm-.jpg"></v-img>
                 </div>
               </v-col>
             </v-row>
