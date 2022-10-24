f5927e4b11209b28e9beb4b7d7d01adcd7a9cfb0
Ximenakdi <ximena_pinacho@icloud.com>
2022-10-24 02:13:57 -0500

    Se añadió barra de busqueda para los articulos, aun no es funcional, sólo existe

--- src/views/Papers.vue
+++ src/views/Papers.vue
@@ -1,6 +1,21 @@
 <template>
   <v-container>
     <v-row align="center" justify="center">
+      <v-col cols="12" sm="9">
+        <v-autocomplete
+          v-model="model"
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          color="FIRed"
+          hide-no-data
+          hide-selected
+          placeholder="Ingresa las palabras clave o el nombre del artículo"
+          flat
+          filled
+          rounded
+        ></v-autocomplete>
+      </v-col>
       <v-col cols="12" sm="10">
         <PaperCard/>
       </v-col>
